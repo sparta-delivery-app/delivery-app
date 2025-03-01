02dce3e07dbc08e6f3d5199b2d95806539f76cac
나연 <kny5579@naver.com>
2025-03-02 00:35:38 +0900

    test: 회원가입 컨트롤러 테스트 코드 작성

---
+++ src/test/java/com/example/deliveryapp/domain/user/controller/UserControllerTest.java
@@ -0,0 +1,49 @@
+package com.example.deliveryapp.domain.user.controller;
+
+import com.example.deliveryapp.domain.user.dto.request.SignUpRequest;
+import com.example.deliveryapp.domain.user.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBeans;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(UserController.class)
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockitoBean
+    private UserService userService;
+
+    @Test
+    void 회원가입_성공() throws Exception {
+
+        SignUpRequest request = new SignUpRequest("em@em.com","pw","name","OWNER");
+
+        String token = "BearerToken";
+        given(userService.signUp(Mockito.any(SignUpRequest.class))).willReturn(token);
+
+        mockMvc.perform(post("/users/signup")
+                        .content(objectMapper.writeValueAsString(request))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(token));
+    }
+
+}
