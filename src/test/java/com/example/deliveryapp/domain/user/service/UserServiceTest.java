7a0f865532be4f701b6cc90d20f4afaaf8ac3cdd
나연 <kny5579@naver.com>
2025-03-03 19:47:50 +0900

    test: 로그인, 프로필 조회, 회원 탈퇴 서비스 테스트 코드 작성

--- src/test/java/com/example/deliveryapp/domain/user/service/UserServiceTest.java
+++ src/test/java/com/example/deliveryapp/domain/user/service/UserServiceTest.java
@@ -3,8 +3,12 @@ package com.example.deliveryapp.domain.user.service;
 import com.example.deliveryapp.config.JwtUtil;
 import com.example.deliveryapp.config.PasswordEncoder;
 import com.example.deliveryapp.domain.common.exception.CustomException;
+import com.example.deliveryapp.domain.user.dto.request.SignInRequest;
 import com.example.deliveryapp.domain.user.dto.request.SignUpRequest;
+import com.example.deliveryapp.domain.user.dto.request.UserDeleteRequest;
+import com.example.deliveryapp.domain.user.dto.response.SignInResponse;
 import com.example.deliveryapp.domain.user.dto.response.SignUpResponse;
+import com.example.deliveryapp.domain.user.dto.response.UserResponse;
 import com.example.deliveryapp.domain.user.entity.User;
 import com.example.deliveryapp.domain.user.enums.UserRole;
 import com.example.deliveryapp.domain.user.repository.UserRepository;
@@ -15,6 +19,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
@@ -38,10 +44,9 @@ public class UserServiceTest {
     private UserService userService;
 
     @Nested
-    class signup {
+    class signUp {
         @Test
         void 이미_존재하는_이메일로_회원가입시_예외_발생() {
-
             SignUpRequest signUpRequest = new SignUpRequest("email@email.com","password","name","OWNER");
 
             given(userRepository.existsByEmailAndDeletedAtIsNull(anyString())).willReturn(true);
@@ -51,7 +56,6 @@ public class UserServiceTest {
 
         @Test
         void 탈퇴한_이메일로_회원가입시_예외_발생() {
-
             SignUpRequest signUpRequest = new SignUpRequest("email@email.com","password","name","OWNER");
 
             given(userRepository.existsByEmailAndDeletedAtIsNull(anyString())).willReturn(false);
@@ -62,7 +66,6 @@ public class UserServiceTest {
 
         @Test
         void 회원가입_성공() {
-
             long userId = 1L;
 
             SignUpRequest signUpRequest = new SignUpRequest("email@email.com","password","name","OWNER");
@@ -86,6 +89,112 @@ public class UserServiceTest {
 
             assertNotNull(signUpResponse);
             assertEquals("BearerToken", signUpResponse.getBearerToken());
+            verify(userRepository, times(1)).save(any(User.class));
+        }
+    }
+
+    @Nested
+    class signIn {
+        @Test
+        void 존재하지_않는_이메일로_로그인시_예외_발생() {
+            SignInRequest signInRequest = new SignInRequest("email@email.com","password");
+
+            given(userRepository.findByEmail(anyString())).willReturn(Optional.empty());
+
+            assertThrows(CustomException.class,()->userService.signIn(signInRequest),"사용자를 찾을 수 없습니다");
+        }
+
+        @Test
+        void 비밀번호_불일치시_예외_발생() {
+            long userId = 1L;
+            User user = new User("email@email.com","password","name",UserRole.OWNER);
+            SignInRequest signInRequest = new SignInRequest("email@email.com","password");
+            ReflectionTestUtils.setField(user,"id",userId);
+
+            given(userRepository.findByEmail(anyString())).willReturn(Optional.of(user));
+            given(passwordEncoder.matches(anyString(),anyString())).willReturn(false);
+
+            assertThrows(CustomException.class,()->userService.signIn(signInRequest),"비밀번호가 올바르지 않습니다.");
+        }
+
+        @Test
+        void 로그인_성공() {
+            long userId = 1L;
+            User user = new User("email@email.com","password","name",UserRole.OWNER);
+            SignInRequest signInRequest = new SignInRequest("email@email.com","password");
+            ReflectionTestUtils.setField(user,"id",userId);
+
+            given(userRepository.findByEmail(anyString())).willReturn(Optional.of(user));
+            given(passwordEncoder.matches(anyString(),anyString())).willReturn(true);
+            given(jwtUtil.createToken(anyLong(), anyString(), anyString(), any(UserRole.class))).willReturn("BearerToken");
+
+            SignInResponse signInResponse = userService.signIn(signInRequest);
+
+            assertNotNull(signInResponse);
+            assertEquals("BearerToken", signInResponse.getBearerToken());
+        }
+    }
+
+    @Nested
+    class getUser {
+        @Test
+        void 사용자_id가_존재하지_않을시_예외_발생() {
+            long userId = 1L;
+
+            given(userRepository.findById(anyLong())).willReturn(Optional.empty());
+
+            assertThrows(CustomException.class,()->userService.getUser(userId),"사용자를 찾을 수 없습니다");
+        }
+
+        @Test
+        void 사용자_id로_프로필_조회_성공() {
+            long userId = 1L;
+            String email = "email@email.com";
+            User user = new User(email,"password","name",UserRole.OWNER);
+            ReflectionTestUtils.setField(user,"id",userId);
+
+            given(userRepository.findById(anyLong())).willReturn(Optional.of(user));
+
+            UserResponse userResponse = userService.getUser(userId);
+
+            assertNotNull(userResponse);
+            assertEquals(userResponse.getId(),userId);
+            assertEquals(userResponse.getEmail(),email);
+        }
+    }
+
+    @Nested
+    class deleteUser {
+        @Test
+        void 탈퇴_비밀번호_불일치시_예외_발생() {
+            long userId = 1L;
+            String email = "email@email.com";
+            User user = new User(email, "password", "name", UserRole.OWNER);
+            ReflectionTestUtils.setField(user, "id", userId);
+
+            UserDeleteRequest userDeleteRequest = new UserDeleteRequest("password");
+
+            given(userRepository.findById(anyLong())).willReturn(Optional.of(user));
+            given(passwordEncoder.matches(anyString(), anyString())).willReturn(false);
+
+            assertThrows(CustomException.class,()->userService.deleteUser(userId,userDeleteRequest),"비밀번호가 올바르지 않습니다.");
+        }
+
+        @Test
+        void 사용자_탈퇴_성공() {
+            long userId = 1L;
+            String email = "email@email.com";
+            User user = new User(email, "password", "name", UserRole.OWNER);
+            ReflectionTestUtils.setField(user, "id", userId);
+
+            UserDeleteRequest userDeleteRequest = new UserDeleteRequest("password");
+
+            given(userRepository.findById(anyLong())).willReturn(Optional.of(user));
+            given(passwordEncoder.matches(anyString(), anyString())).willReturn(true);
+
+            userService.deleteUser(userId, userDeleteRequest);
+
+            assertNotNull(user.getDeletedAt());
             verify(userRepository, times(1)).save(any(User.class));
         }
     }
