7b60575f87ae5e85708fc84238254f8930dc27c1
나연 <kny5579@naver.com>
2025-03-04 01:25:21 +0900

    feat: 주문 생성 서비스 로직 작성

--- src/main/java/com/example/deliveryapp/domain/order/service/OrderService.java
+++ src/main/java/com/example/deliveryapp/domain/order/service/OrderService.java
@@ -1,9 +1,49 @@
 package com.example.deliveryapp.domain.order.service;
 
+import com.example.deliveryapp.domain.common.exception.CustomException;
+import com.example.deliveryapp.domain.common.exception.ErrorCode;
+import com.example.deliveryapp.domain.order.dto.request.OrderRequest;
+import com.example.deliveryapp.domain.order.dto.response.OrderResponse;
+import com.example.deliveryapp.domain.order.entity.Order;
+import com.example.deliveryapp.domain.order.entity.OrderMenu;
+import com.example.deliveryapp.domain.order.enums.OrderState;
+import com.example.deliveryapp.domain.order.repository.OrderMenuRepository;
+import com.example.deliveryapp.domain.order.repository.OrderRepository;
+import com.example.deliveryapp.domain.store.entity.Store;
+import com.example.deliveryapp.domain.store.repository.StoreRepository;
+import com.example.deliveryapp.domain.user.entity.User;
+import com.example.deliveryapp.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class OrderService {
+
+    private final UserRepository userRepository;
+    private final StoreRepository storeRepository;
+    private final OrderRepository orderRepository;
+    private final OrderMenuRepository orderMenuRepository;
+
+    @Transactional
+    public OrderResponse createOrder(Long userId, OrderRequest orderRequest) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(()->new CustomException(ErrorCode.USER_NOT_FOUND));
+        Store store = storeRepository.findById(orderRequest.getStoreId())
+                .orElseThrow(()->new CustomException(ErrorCode.STORE_NOT_FOUND));
+
+        Order order = new Order(user,store, OrderState.PENDING);
+
+        orderRepository.save(order);
+
+        OrderMenu orderMenu = new OrderMenu(order,
+                orderRequest.getOrderMenus().getMenuId(),
+                orderRequest.getOrderMenus().getName(),
+                orderRequest.getOrderMenus().getPrice());
+
+        orderMenuRepository.save(orderMenu);
+
+        return new OrderResponse(order, orderMenu);
+    }
 }
