75e78c62f69c6c618e4c0c357f1b5230712af4fc
나연 <kny5579@naver.com>
2025-03-04 19:36:16 +0900

    refactor: validation 추가

--- src/main/java/com/example/deliveryapp/domain/order/dto/request/OrderMenuRequest.java
+++ src/main/java/com/example/deliveryapp/domain/order/dto/request/OrderMenuRequest.java
@@ -1,12 +1,19 @@
 package com.example.deliveryapp.domain.order.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class OrderMenuRequest {
+
+    @NotBlank
     private Long menuId;
+
+    @NotBlank
     private String name;
+
+    @NotBlank
     private Long price;
 }
--- src/main/java/com/example/deliveryapp/domain/order/dto/request/OrderRequest.java
+++ src/main/java/com/example/deliveryapp/domain/order/dto/request/OrderRequest.java
@@ -1,13 +1,20 @@
 package com.example.deliveryapp.domain.order.dto.request;
 
 import com.example.deliveryapp.domain.order.enums.OrderState;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class OrderRequest {
+
+    @NotBlank
     private Long storeId;
+
+    @NotBlank
     private OrderState orderState;
+
+    @NotBlank
     private OrderMenuRequest orderMenus;
 }
