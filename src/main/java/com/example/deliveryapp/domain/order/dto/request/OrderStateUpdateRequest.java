59076e2e70b0365c9cce44ef12421a0ee40b0fc8
나연 <kny5579@naver.com>
2025-03-04 19:36:16 +0900

    refactor: validation 추가

--- src/main/java/com/example/deliveryapp/domain/order/dto/request/OrderStateUpdateRequest.java
+++ src/main/java/com/example/deliveryapp/domain/order/dto/request/OrderStateUpdateRequest.java
@@ -1,11 +1,14 @@
 package com.example.deliveryapp.domain.order.dto.request;
 
 import com.example.deliveryapp.domain.order.enums.OrderState;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class OrderStateUpdateRequest {
+
+    @NotBlank
     private OrderState orderState;
 }
