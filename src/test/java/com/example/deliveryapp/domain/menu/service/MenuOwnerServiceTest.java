5c381e15f271739097cb07a761d015d0ef214050
iboyeon0816 <iboyeon0816@gmail.com>
2025-03-04 13:29:14 +0900

    test: MenuOwnerService의 updateMenu 메서드 테스트 코드 작성

--- src/test/java/com/example/deliveryapp/domain/menu/service/MenuOwnerServiceTest.java
+++ src/test/java/com/example/deliveryapp/domain/menu/service/MenuOwnerServiceTest.java
@@ -3,6 +3,7 @@ package com.example.deliveryapp.domain.menu.service;
 import com.example.deliveryapp.domain.common.exception.CustomException;
 import com.example.deliveryapp.domain.common.exception.ErrorCode;
 import com.example.deliveryapp.domain.menu.dto.request.MenuSaveRequest;
+import com.example.deliveryapp.domain.menu.dto.request.MenuUpdateRequest;
 import com.example.deliveryapp.domain.menu.dto.response.MenuResponse;
 import com.example.deliveryapp.domain.menu.entity.Menu;
 import com.example.deliveryapp.domain.menu.repository.MenuRepository;
@@ -37,17 +38,18 @@ class MenuOwnerServiceTest {
     @InjectMocks
     private MenuOwnerService menuOwnerService;
 
-    private MenuSaveRequest request;
-
-    @BeforeEach
-    void setUp() {
-        request = new MenuSaveRequest("menu1", 15000L);
-    }
 
     @Nested
     @Order(1)
     @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-    class saveMenuTests {
+    class SaveMenuTests {
+
+        private MenuSaveRequest request;
+
+        @BeforeEach
+        void setUp() {
+            request = new MenuSaveRequest("menu1", 15000L);
+        }
 
         @Test
         @Order(1)
@@ -109,6 +111,115 @@ class MenuOwnerServiceTest {
             verify(menuRepository, times(1)).save(any(Menu.class));
 
             assertNotNull(response);
+            assertEquals(request.getMenuName(), response.getMenuName());
+            assertEquals(request.getPrice(), response.getPrice());
+        }
+    }
+
+    @Nested
+    @Order(2)
+    @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+    class UpdateMenuTests {
+
+        private MenuUpdateRequest request;
+
+        @BeforeEach
+        void setUp() {
+            request = new MenuUpdateRequest("menu1", 15000L);
+        }
+
+        @Test
+        @Order(1)
+        void 메뉴수정_주인_권한_없음_실패() {
+            // given
+            User mockUser = mock(User.class);
+            when(mockUser.getRole()).thenReturn(UserRole.USER);
+            when(userRepository.findActiveUserByIdOrThrow(anyLong())).thenReturn(mockUser);
+
+            // when & then
+            CustomException thrown = assertThrows(CustomException.class,
+                    () -> menuOwnerService.updateMenu(1L, 1L, 1L, request)
+            );
+
+            assertEquals(ErrorCode.OWNER_ONLY_ACCESS, thrown.getErrorCode());
+        }
+
+        @Test
+        @Order(2)
+        void 메뉴수정_가게_주인_아님_실패() {
+            // given
+            User mockUser = mock(User.class);
+            when(mockUser.getId()).thenReturn(1L);
+            when(mockUser.getRole()).thenReturn(UserRole.OWNER);
+            when(userRepository.findActiveUserByIdOrThrow(anyLong())).thenReturn(mockUser);
+
+            Store mockStore = mock(Store.class);
+            when(storeRepository.findActiveStoreByIdOrThrow(anyLong())).thenReturn(mockStore);
+
+            User storeOwnerUser = mock(User.class);
+            when(storeOwnerUser.getId()).thenReturn(2L);
+            when(mockStore.getUser()).thenReturn(storeOwnerUser);
+
+            // when & then
+            CustomException thrown = assertThrows(CustomException.class,
+                    () -> menuOwnerService.updateMenu(mockUser.getId(), mockStore.getId(), 1L, request)
+            );
+            assertEquals(ErrorCode.NOT_STORE_OWNER, thrown.getErrorCode());
+        }
+
+        @Test
+        @Order(3)
+        void 메뉴수정_가게_다름_실패() {
+            // given
+            User mockUser = mock(User.class);
+            when(mockUser.getId()).thenReturn(1L);
+            when(mockUser.getRole()).thenReturn(UserRole.OWNER);
+            when(userRepository.findActiveUserByIdOrThrow(anyLong())).thenReturn(mockUser);
+
+            Store mockStore = mock(Store.class);
+            when(mockStore.getId()).thenReturn(1L);
+            when(mockStore.getUser()).thenReturn(mockUser);
+            when(storeRepository.findActiveStoreByIdOrThrow(anyLong())).thenReturn(mockStore);
+
+            Store anotherStore = mock(Store.class);
+            when(anotherStore.getId()).thenReturn(2L);
+
+            Menu mockMenu = mock(Menu.class);
+            when(mockMenu.getStore()).thenReturn(anotherStore);
+            when(menuRepository.findActiveMenuByIdOrThrow(anyLong())).thenReturn(mockMenu);
+
+            // when & then
+            CustomException thrown = assertThrows(CustomException.class,
+                    () -> menuOwnerService.updateMenu(mockUser.getId(), mockStore.getId(), 1L, request)
+            );
+            assertEquals(ErrorCode.NOT_STORE_MENU, thrown.getErrorCode());
+        }
+
+        @Test
+        @Order(4)
+        void 메뉴수정_성공() {
+            // given
+            User mockUser = mock(User.class);
+            when(mockUser.getId()).thenReturn(1L);
+            when(mockUser.getRole()).thenReturn(UserRole.OWNER);
+            when(userRepository.findActiveUserByIdOrThrow(anyLong())).thenReturn(mockUser);
+
+            Store mockStore = mock(Store.class);
+            when(mockStore.getId()).thenReturn(1L);
+            when(mockStore.getUser()).thenReturn(mockUser);
+            when(storeRepository.findActiveStoreByIdOrThrow(anyLong())).thenReturn(mockStore);
+
+            Menu mockMenu = spy(Menu.class);
+            when(mockMenu.getId()).thenReturn(1L);
+            when(mockMenu.getStore()).thenReturn(mockStore);
+            when(menuRepository.findActiveMenuByIdOrThrow(anyLong())).thenReturn(mockMenu);
+
+            // when
+            MenuResponse response = menuOwnerService.updateMenu(mockUser.getId(), mockStore.getId(), mockMenu.getId(), request);
+
+            // then
+            assertNotNull(response);
+            assertEquals(mockMenu.getId(), response.getMenuId());
             assertEquals(request.getMenuName(), response.getMenuName());
             assertEquals(request.getPrice(), response.getPrice());
         }
