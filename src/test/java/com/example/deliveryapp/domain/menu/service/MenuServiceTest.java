88ff0582d3719e4c6c1541c473cfc5c7ad68faff
iboyeon0816 <iboyeon0816@gmail.com>
2025-03-05 16:46:44 +0900

    test: 메뉴 이미지 API 관련 테스트 코드 작성
    - 기존 메뉴 서비스 테스트 코드 수정
    - 메뉴 이미지 관련 서비스 테스트 코드 추가

--- src/test/java/com/example/deliveryapp/domain/menu/service/MenuServiceTest.java
+++ src/test/java/com/example/deliveryapp/domain/menu/service/MenuServiceTest.java
@@ -1,9 +1,10 @@
 package com.example.deliveryapp.domain.menu.service;
 
+import com.example.deliveryapp.client.S3Service;
 import com.example.deliveryapp.domain.common.exception.CustomException;
 import com.example.deliveryapp.domain.common.exception.ErrorCode;
 import com.example.deliveryapp.domain.menu.dto.response.MenuPageResponse;
-import com.example.deliveryapp.domain.menu.dto.response.MenuResponse;
+import com.example.deliveryapp.domain.menu.entity.Menu;
 import com.example.deliveryapp.domain.menu.repository.MenuRepository;
 import com.example.deliveryapp.domain.store.repository.StoreRepository;
 import org.junit.jupiter.api.*;
@@ -16,9 +17,10 @@ import org.springframework.data.domain.*;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @TestClassOrder(ClassOrderer.OrderAnnotation.class)
 @ExtendWith(MockitoExtension.class)
@@ -29,6 +31,9 @@ class MenuServiceTest {
 
     @Mock
     private StoreRepository storeRepository;
+
+    @Mock
+    private S3Service s3Service;
 
     @InjectMocks
     private MenuService menuService;
@@ -55,24 +60,28 @@ class MenuServiceTest {
         void 메뉴조회_성공() {
             // given
             Integer page = 1, size = 10;
-            List<MenuResponse> menuResponseList = List.of(
-                    new MenuResponse(1L, "menu1", 5000L, "description1", null),
-                    new MenuResponse(2L, "menu2", 15000L, "description2", null)
-            );
+            List<Menu> menuList = List.of( mock(Menu.class), mock(Menu.class));
+            when(menuList.get(0).getImageUrl()).thenReturn("unsignedUrl");
+            String signedUrl = "signedUrl";
+
             Pageable pageable = PageRequest.of(page - 1, size, Sort.by("name").ascending());
-            Page<MenuResponse> menuPage = new PageImpl<>(menuResponseList, pageable, menuResponseList.size());
+            Page<Menu> menuPage = new PageImpl<>(menuList, pageable, menuList.size());
 
             given(storeRepository.existsByIdAndDeletedAtIsNull(anyLong())).willReturn(true);
-            given(menuRepository.findAllByStoreId(anyLong(), any(Pageable.class))).willReturn(menuPage);
+            given(menuRepository.findAllByStoreIdAndDeletedAtIsNull(anyLong(), any(Pageable.class))).willReturn(menuPage);
+            given(s3Service.createSignedUrl(any(), anyString())).willReturn(signedUrl);
 
-            // when & then
+            // when
             MenuPageResponse response = menuService.getMenus(1L, page, size);
+
+            // then
             assertNotNull(response);
-            assertEquals(menuResponseList.size(), response.getContent().size());
-            assertEquals(menuResponseList.size(), response.getTotalElements());
+            assertEquals(menuList.size(), response.getContent().size());
+            assertEquals(menuList.size(), response.getTotalElements());
             assertEquals(1, response.getTotalPages());
             assertEquals(page, response.getCurrentPage());
             assertEquals(size, response.getSize());
+            assertEquals(signedUrl, response.getContent().get(0).getImageUrl());
         }
     }
 }
