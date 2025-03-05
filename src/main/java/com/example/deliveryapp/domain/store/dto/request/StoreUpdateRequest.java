035d7456785b1cb007a0d93902a87fb06bc1bd1a
karyosome1 <ammonite0880@naver.com>
2025-03-05 20:11:43 +0900

    refactor: validation 추가

--- src/main/java/com/example/deliveryapp/domain/store/dto/request/StoreUpdateRequest.java
+++ src/main/java/com/example/deliveryapp/domain/store/dto/request/StoreUpdateRequest.java
@@ -1,15 +1,33 @@
 package com.example.deliveryapp.domain.store.dto.request;
 
+import com.example.deliveryapp.domain.store.consts.Const;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class StoreUpdateRequest {
 
+    @NotBlank(message = Const.STORE_NAME_NOT_NULL)
+    @Size(max = 20, message = Const.STORE_NAME_SIZE)
     private final String name;
+
+    @NotBlank(message = Const.OPEN_TIME_NOT_NULL)
+    @Pattern(regexp = Const.OPEN_TIME_PATTERN, message = Const.OPEN_TIME_REQUIREMENT)
     private final String openTime;
+
+    @NotBlank(message = Const.CLOSE_TIME_NOT_NULL)
+    @Pattern(regexp = Const.CLOSE_TIME_PATTERN, message = Const.CLOSE_TIME_REQUIREMENT)
     private final String closeTime;
+
+    @NotBlank(message = Const.MINIMUM_ORDER_PRICE_NOT_NULL)
+    @Min(value = 0, message = Const.MINIMUM_ORDER_PRICE_REQUIREMENT)
     private final Long minimumOrderPrice;
+
+    @NotBlank(message = Const.STORE_STATUS_NOT_NULL)
     private final String status;
 
     @Builder
