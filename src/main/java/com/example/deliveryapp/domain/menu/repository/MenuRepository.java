538456137f844afd087775ebafbb82d9a847a3bd
iboyeon0816 <iboyeon0816@gmail.com>
2025-03-05 17:39:42 +0900

    fix: dev와 feature/menu-image 간 충돌 해결

--- src/main/java/com/example/deliveryapp/domain/menu/repository/MenuRepository.java
+++ src/main/java/com/example/deliveryapp/domain/menu/repository/MenuRepository.java
@@ -2,10 +2,13 @@ package com.example.deliveryapp.domain.menu.repository;
 
 import com.example.deliveryapp.domain.common.exception.CustomException;
 import com.example.deliveryapp.domain.common.exception.ErrorCode;
+import com.example.deliveryapp.domain.menu.dto.response.MenuResponse;
 import com.example.deliveryapp.domain.menu.entity.Menu;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -20,7 +23,7 @@ public interface MenuRepository extends JpaRepository<Menu, Long> {
                 .orElseThrow(() -> new CustomException(ErrorCode.MENU_NOT_FOUND));
     }
 
-    @Query("SELECT new com.example.deliveryapp.domain.menu.dto.response.MenuResponse(m.id, m.name, m.price) " +
+    @Query("SELECT new com.example.deliveryapp.domain.menu.dto.response.MenuResponse(m.id, m.name, m.price, m.description) " +
             "FROM Menu m " +
             "WHERE m.store.id = :storeId AND m.deletedAt IS NULL")
     List<MenuResponse> findListByStoreId(@Param("storeId") Long storeId);
