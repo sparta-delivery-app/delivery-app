26933f0a2ecd1bcc925ee73da4217c9402a493d1
나연 <kny5579@naver.com>
2025-03-06 17:31:45 +0900

    test: aop에 따른 컨트롤러 테스트 코드 분할

--- src/test/java/com/example/deliveryapp/domain/order/controller/OrderUserControllerTest.java
+++ src/test/java/com/example/deliveryapp/domain/order/controller/OrderUserControllerTest.java
@@ -3,7 +3,6 @@ package com.example.deliveryapp.domain.order.controller;
 import com.example.deliveryapp.domain.menu.entity.Menu;
 import com.example.deliveryapp.domain.order.converter.OrderConverter;
 import com.example.deliveryapp.domain.order.dto.request.CartAddRequest;
-import com.example.deliveryapp.domain.order.dto.request.OrderStateUpdateRequest;
 import com.example.deliveryapp.domain.order.dto.response.OrderResponse;
 import com.example.deliveryapp.domain.order.entity.Order;
 import com.example.deliveryapp.domain.order.entity.OrderMenu;
@@ -27,16 +26,16 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.time.LocalTime;
 import java.util.List;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doNothing;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(OrderController.class)
-public class OrderControllerTest {
+@WebMvcTest(OrderUserController.class)
+public class OrderUserControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -51,19 +50,75 @@ public class OrderControllerTest {
     private CartService cartService;
 
     @Test
-    void 사장님의_가게별_주문_목록_조회_성공() throws Exception {
+    void 주문_생성_성공() throws Exception {
         Long userId = 1L;
-        User user1 = new User("em@em.com", "pw", "name", UserRole.OWNER);
-
-        Long storeId = 1L;
+        User user = new User("em@em.com", "pw", "name", UserRole.USER);
         Store store = new Store(
                 "name", LocalTime.of(9, 0), LocalTime.of(22, 0),
-                1000L, StoreStatus.OPEN, user1);
-        ReflectionTestUtils.setField(store, "id", storeId);
+                1000L, StoreStatus.OPEN, user);
 
         String bearerToken = "bearerToken";
 
-        Order order = new Order(user1, store, OrderState.PENDING);
+        Order order = new Order(user, store, OrderState.CART);
+        OrderMenu orderMenu = new OrderMenu(new Menu("name1", 1000L, "description", store));
+        ReflectionTestUtils.setField(orderMenu, "id", 1L);
+        OrderMenu orderMenu1 = new OrderMenu(new Menu("name2", 200L, "description", store));
+        ReflectionTestUtils.setField(orderMenu1, "id", 2L);
+        order.addOrderMenu(orderMenu);
+        order.addOrderMenu(orderMenu1);
+
+        OrderResponse orderResponse = OrderConverter.toResponse(order);
+
+        given(orderService.createOrder(anyLong())).willReturn(orderResponse);
+
+        mockMvc.perform(post("/orders")
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + bearerToken)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .with(request -> {
+                            request.setAttribute("userId", userId);
+                            request.setAttribute("email", "em@em.com");
+                            request.setAttribute("name", "name");
+                            request.setAttribute("userRole", "USER");
+                            return request;
+                        }))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void 장바구니_추가_성공() throws Exception {
+        long userId = 1L;
+        long menuId = 1L;
+
+        String bearerToken = "bearerToken";
+
+        CartAddRequest cartAddRequest = new CartAddRequest(menuId, null);
+        doNothing().when(cartService).addCart(userId,cartAddRequest);
+
+        mockMvc.perform(post("/carts",menuId)
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + bearerToken)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(cartAddRequest))
+                        .with(request -> {
+                            request.setAttribute("userId", userId);
+                            request.setAttribute("email", "em@em.com");
+                            request.setAttribute("name", "name");
+                            request.setAttribute("userRole", "USER");
+                            return request;
+                        }))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void 사용자의_주문_목록_조회_성공() throws Exception {
+        Long userId = 1L;
+        User user = new User("em@em.com", "pw", "name", UserRole.USER);
+        Store store = new Store(
+                "name", LocalTime.of(9, 0), LocalTime.of(22, 0),
+                1000L, StoreStatus.OPEN, user);
+
+        String bearerToken = "bearerToken";
+
+        Order order = new Order(user, store, OrderState.PENDING);
 
         OrderMenu orderMenu = new OrderMenu(new Menu("menu1",10000L, "description", store));
         OrderMenu orderMenu2 = new OrderMenu(new Menu("menu2",10000L, "description", store));
@@ -73,9 +128,9 @@ public class OrderControllerTest {
 
         List<OrderResponse> orderList = List.of(response);
 
-        given(orderService.getOrdersByStoreId(anyLong(), anyLong())).willReturn(orderList);
+        given(orderService.getOrdersByUserId(anyLong())).willReturn(orderList);
 
-        mockMvc.perform(get("/stores/{storeId}/orders", storeId)
+        mockMvc.perform(get("/orders")
                         .header(HttpHeaders.AUTHORIZATION, "Bearer " + bearerToken)
                         .contentType(MediaType.APPLICATION_JSON)
                         .with(request -> {
@@ -87,29 +142,5 @@ public class OrderControllerTest {
                         }))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.length()").value(1));
-    }
-
-    @Test
-    void 주문_상태_변경_성공() throws Exception {
-        Long userId = 1L;
-        Long orderId = 1L;
-        String bearerToken = "bearerToken";
-
-        OrderStateUpdateRequest stateUpdateRequest = new OrderStateUpdateRequest(OrderState.ACCEPTED);
-
-        doNothing().when(orderService).updateOrderState(anyLong(), anyLong(), any(OrderStateUpdateRequest.class));
-
-        mockMvc.perform(patch("/orders/{orderId}", orderId)
-                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + bearerToken)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(stateUpdateRequest))
-                        .with(request -> {
-                            request.setAttribute("userId", userId);
-                            request.setAttribute("email", "em@em.com");
-                            request.setAttribute("name", "name");
-                            request.setAttribute("userRole", "USER");
-                            return request;
-                        }))
-                .andExpect(status().isOk());
     }
 }
